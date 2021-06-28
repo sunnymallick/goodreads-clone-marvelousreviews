9d1f01516522a50d73f13ed85310c18c0af69bff
Sunny Mallick <sunny.mallick927@gmail.com>
2021-06-28 14:38:02 -0600

    finished signup page

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(usersRouter);
 
 // set up session middleware
 const store = new SequelizeStore({ db: sequelize });
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+// const csrf = require('csurf')
+// const { csrfProtection, asyncHandler } = require('./utils');
+const { User } = require('../db/models/user')
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next)
+
+router.get('/user/signup', (req, res) => {
+  // const user = User.create();
+  res.render('user-signup')
+})
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
