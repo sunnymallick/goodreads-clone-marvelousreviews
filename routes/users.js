36a9a08f104f8f98dd31ac9ecef428cbc6a60a65
Sunny Mallick <sunny.mallick927@gmail.com>
2021-06-28 15:01:32 -0600

    finished signup page

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const router = express.Router();
-// const csrf = require('csurf')
-// const { csrfProtection, asyncHandler } = require('./utils');
-const { User } = require('../db/models/user')
-const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next)
+const { csrfProtection, asyncHandler } = require('./utils');
+const { User } = require('../db/models')
 
-router.get('/user/signup', (req, res) => {
-  // const user = User.create();
-  res.render('user-signup')
+router.get('/user/signup', csrfProtection, (req, res) => {
+  res.render('user-signup', { token: req.csrfToken() })
 })
 
+router.post('/user/signup', csrfProtection, asyncHandler( async (req, res) => {
+  const { username, email, password } = req.body
+  const user = await User.create({
+    username,
+    email,
+    password,
+  })
+  res.render('/user/profile', { user })
+}))
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/user/profile', (req, res) => {
+  res.send("Welcome to the user's profile!")
+})
 
 module.exports = router;
--- routes/utils.js
+++ routes/utils.js
@@ -1,5 +1,5 @@
-// const csrf = require('csurf');
-// const csrfProtection = csrf({cookie: true});
+const csrf = require('csurf');
+const csrfProtection = csrf({cookie: true});
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 
