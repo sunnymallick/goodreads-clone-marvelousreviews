3c68df4e62290635c00a45d3cde5012f76f51576
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-06-29 19:36:51 -0400

    MoviesPage1.0

---
+++ routes/movies.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { Movie } = require('../db/models');
+
+const db = require('../db/models');
+
+router.post('/movies', (req, res) =>{
+
+})
