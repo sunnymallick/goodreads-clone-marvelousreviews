f967ca21ab7ac6ad9b3541d7eb8ef8292995b706
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-07-01 18:21:46 -0400

    ShelfSeedingxAssociations

--- db/models/movieshelf.js
+++ db/models/movieshelf.js
@@ -1,12 +1,15 @@
 'use strict';
+
+const { BelongsTo } = require("sequelize/types");
+
 module.exports = (sequelize, DataTypes) => {
   const MovieShelf = sequelize.define('MovieShelf', {
     user_id: DataTypes.INTEGER,
     title: DataTypes.STRING,
-    
+
   }, {});
   MovieShelf.associate = function(models) {
-    // associations can be defined here
+    MovieShelf.belongsTo(models.User, {foreignKey: 'user_id'})
   };
   return MovieShelf;
 };
