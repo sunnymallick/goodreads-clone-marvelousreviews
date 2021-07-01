8ade574d487667356a97ac8b58abc762e3f31d9d
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-07-01 19:35:35 -0400

    Shelf3.0

--- db/models/movieshelf.js
+++ db/models/movieshelf.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   MovieShelf.associate = function(models) {
     MovieShelf.belongsTo(models.User, {foreignKey: 'user_id'})
+    MovieShelf.hasMany(models.Movie, {foreignKey: 'movie_id'})
   };
   return MovieShelf;
 };
